4df25af455521d433637da949113997e752c4a2f
Matt Jeanes <matthew.jeanes@moneybarn.com>
2018-07-31 12:24:52 +0100

    ngx-parallax example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,8 @@ import { SupportPage } from '../pages/support/support';
 import { ConferenceData } from '../providers/conference-data';
 import { UserData } from '../providers/user-data';
 
+import { ParallaxModule } from "ngx-parallax";
+
 
 @NgModule({
   declarations: [
@@ -46,10 +48,11 @@ import { UserData } from '../providers/user-data';
     SpeakerListPage,
     TabsPage,
     TutorialPage,
-    SupportPage
+    SupportPage,
   ],
   imports: [
     BrowserModule,
+    ParallaxModule,
     HttpModule,
     IonicModule.forRoot(ConferenceApp, {}, {
       links: [
